8a618bb5e346517f9dd00390d0b2cfcf4c3e95a5
baothanh <hlb.thanh20091998@gmail.com>
2021-08-07 16:13:35 +0700

    test branch hoang-trinh

--- src/module/main/components/theater-list/theater-list.component.jsx
+++ src/module/main/components/theater-list/theater-list.component.jsx
@@ -5,58 +5,94 @@ import { getTheaterCinemaAction, getTheaterListAction, getTheaterShowTimeAction 
 import './theater-list.style.scss'
 
 export default function TheaterList() {
+    const [phimTheoCumRap, setPhimTheoCumRap] = useState(
+        "bhd-star-cineplex-3-2"
+    );
+
     const dispatch = useDispatch();
     useEffect(() => {
         handleChoiceCinema("BHDStar")
-        handleChoiceListCinema("BHDStar")
         dispatch(getTheaterListAction())
+        renderCinema()
+
     }, [])
     const theaterList = useSelector((state) => state.theater.theaterList)
     const theaterCinema = useSelector((state) => state.theater.theaterCinema)
     const listCumRap = useSelector((state) => state.theater.theaterShowTime[0]?.lstCumRap)
     const handleChoiceCinema = (cinema) => {
         dispatch(getTheaterCinemaAction(cinema))
+        dispatch(getTheaterShowTimeAction(cinema))
+
     }
     //Lấy ra cụm rạp và trỏ tới nơi cụm rạp
-    const handleChoiceListCinema = (cinema) => {
-        dispatch(getTheaterShowTimeAction(cinema))
-        console.log(listCumRap);
-    }
-    let rap = []
-    const handleChoiceCinemaShowTime = (e) => {
-        rap = listCumRap?.filter(cinema => cinema.maCumRap === e)
-        console.log(rap[0]?.danhSachPhim);
-    }
 
-
-    const renderLogo = theaterList.map((theater, index) => (
+    const renderLogo = () => theaterList.map((theater, index) => (
         <div className="logo__detail" key={index} >
-            <img src={theater.logo} alt="" onClick={() => (handleChoiceCinema(theater.maHeThongRap), handleChoiceListCinema(theater.maHeThongRap))} />
+            <img src={theater.logo} alt="" onClick={() => (handleChoiceCinema(theater.maHeThongRap))} />
         </div>
     ))
-    const renderCinema = theaterCinema.map((cinema, index) => (
-        <div className="cinema__detail" onClick={() => handleChoiceCinemaShowTime(cinema.maCumRap)}>
+    const renderCinema = () => theaterCinema.map((cinema, index) => (
+        <div
+            className='cinema__detail'
+            onClick={() => setPhimTheoCumRap(cinema.maCumRap)}
+        >
             <p>{cinema.tenCumRap}</p>
             <p>{cinema.diaChi}</p>
-        </div >
-    ))
-    const renderTime = rap[0]?.danhSachPhim.map((ra, index) => (
-        <div>
-            <p>{ra.tenPhim}</p>
-            {console.log(ra.tenPhim)}
         </div>
-    ))
+    ));
+    const timeMovieR = (cine) => {
+        return cine.map((r, index) => {
+            return (
+                <>
+                    <span>{r.ngayChieuGioChieu}</span>
+                    {console.log("Đây là ngày chiếu giờ chiếu r: ", r.ngayChieuGioChieu)}
+                </>
+            )
+        })
+    }
+    const rap = listCumRap?.filter(
+        (cinema) => cinema.maCumRap === phimTheoCumRap
+    );
+    const renderShowTime = () => {
+
+        console.log(rap);
+        console.log(listCumRap);
+        if (rap) {
+            if (rap.length > 0) {
+                return rap[0]?.danhSachPhim.map((ra, index) => {
+                    return (
+                        <div>
+                            <p>{ra.tenPhim}</p>
+                            {console.log("đây là tên phim ra: ", ra.tenPhim)}
+                            {timeMovieR(ra.lstLichChieuTheoPhim)}
+                        </div>
+                    );
+                })
+            } else {
+                return (
+                    <div>
+                        <p>Không có phim</p>
+
+                    </div>
+                )
+            }
+        }
+
+
+    }
 
     return (
         <div className="theater__content">
             <div className="theater__logo">
-                {renderLogo}
+                {renderLogo()}
             </div>
             <div className="theater__cinema">
-                {renderCinema}
+                {renderCinema()}
             </div>
             <div className="theater__showtime">
-                {renderTime}
+                {/* {renderTime()} */}
+                <h1>Showing Time</h1>
+                {renderShowTime()}
             </div>
         </div>
     )
