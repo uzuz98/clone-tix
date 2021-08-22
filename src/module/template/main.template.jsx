3f0830796b75e114fdc7dd110b855fd0f7b9b17b
baothanh <hlb.thanh20091998@gmail.com>
2021-08-22 20:31:07 +0700

    bao-thanh complete code

--- src/module/template/main.template.jsx
+++ src/module/template/main.template.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../main/components/footer/footer.component';
 import Header from '../main/components/header/header.page'
+import LoadingScreen from '../main/components/loading-screen/loadgin-screen.component';
 
 export default function MainTemplates(props) {
     const { children } = props;
+    const [isLoading, setIsLoading] = useState(false)
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(true)
+        }, 2000);
+    }, [])
     return (
         <>
             <header>
                 <Header />
             </header>
             <main>
-                {children}
+                {!isLoading ?
+                    <LoadingScreen /> :
+                    children
+                }
             </main>
             <footer className="footer">
                 <Footer></Footer>
